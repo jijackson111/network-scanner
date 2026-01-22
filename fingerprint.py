cb2ceade47d6acc31b13655ccb3c30344ff4f7df
Josiah Jackson <josiahjackson111@gmail.com>
2026-01-22 18:29:40 +1100

    Added standard service to fingerprint scanner. Port scan/service not running well in main file

--- fingerprint.py
+++ fingerprint.py
@@ -5,8 +5,6 @@ def grab_banner(ip, port):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(2)
         s.connect((ip, port))
-        if port == 80:
-            s.send(b"HEAD / HTTP/1.1\r\nHost: google.com\r\n\r\n")
         banner = s.recv(1024)
         return banner.decode(errors='ignore').strip()
     
@@ -15,3 +13,19 @@ def grab_banner(ip, port):
     
     finally:
         s.close()
+
+def get_standard(port):
+    try:
+        return socket.getservbyport(port, "tcp").upper()
+    except:
+        return "Unknown"
+    
+def get_service(found_ports):
+    ip_port_service_dict = {}
+    for k, v in found_ports.items():
+        port_service_dict = {}
+        for port in v:
+            service = get_standard(port)
+            port_service_dict[port] = service
+        ip_port_service_dict[k] = port_service_dict
+    return ip_port_service_dict
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import sys
 import discovery
 import scanner
+import fingerprint
 
 # Check if run with sudo
 if os.geteuid() != 0:
@@ -18,13 +19,29 @@ target_range = args.t
 port_range = args.p
 output_file = args.o
 
-# Get list of client IP addresses that are up
+# Get list of client IP addresses that are up if range
 active_clients = discovery.scan(target_range)
 ip_list = [client['ip'] for client in active_clients]
+mac_list = [client['mac'] for client in active_clients]
 if not ip_list:
     sys.exit("No active hosts found") 
 print(f"{len(ip_list)} hosts found")
 print(ip_list)
+
+# Check if all ips want to be scanned
+all_ips = input("\nDo you want all IP addresses to be scan [y/n]: ")
+if all_ips == 'y':
+    ip_list_updated = ip_list
+elif all_ips == "n":
+    selected_ips = input("Please enter the index of the IP addresses you would like to use: ")
+    if " " not in selected_ips:
+        ip_list_updated = [ip_list[int(selected_ips)]]
+    else:
+        sips_list = selected_ips.split(" ")
+        ip_list_updated = [ip_list[int(i)] for i in sips_list]
+
 # Send list for scanning
-found_ports = scanner.run_port_scan(ip_list, port_range)
+found_ports = scanner.run_port_scan(ip_list_updated, port_range)
 print(found_ports)
+
+# Retrieve banner
